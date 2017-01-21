b4d05e19fb64389fe72e5940110efa826140161f
joke <k.joke.stone@gmail.com>
2017-01-21 16:48:49 +0800

    no message

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app,os.path.join(basedir,'temp'))
+
+db = SQLAlchemy(app)
+from app import views,models
